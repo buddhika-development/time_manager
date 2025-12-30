6f676ca9e6905cedc9c87b13e430516e5ca2d766
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2025-12-31 01:30:32 +0530

    setup the chat interface with the streaming output

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
+import streamlit as st
 from src.factories.llm_factory import LLMFactory
 
-input = "hello"
+initial_chat_message = [
+    {
+        "role" : "assistant",
+        "content" : "How can I help you to manage your time ?"
+    }
+]
 
-llm = LLMFactory.create("mistral")
+llm = LLMFactory.create("gemini")
 
-print(llm.invoke(input).content)
+def session_state_init():
+    if  "message_history" not in st.session_state:
+        st.session_state.message_history = initial_chat_message
+
+def main():
+    session_state_init()
+
+    # display the initial message
+    for message in st.session_state.message_history:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if user_input := st.chat_input("Ask a question about time management"):
+        with st.chat_message("user"):
+            st.write(user_input)
+        st.session_state.message_history.append({"role": "user", "content": user_input})
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+
+            with st.spinner("Thinking..."):
+                for chunk in llm.stream(user_input):
+                    if hasattr(chunk, 'content'):
+                        full_response += chunk.content
+                        message_placeholder.write(full_response)
+                    else:
+                        full_response = str(chunk)
+
+if __name__ == "__main__":
+    main()
